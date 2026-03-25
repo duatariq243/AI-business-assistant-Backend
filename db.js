2c2c1fac5ef66a1d13647cfafa584481740fcfbc
Dua Tariq <duatariq243@gmail.com>
2026-03-25 11:47:12 -0400

    change

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const { Pool } = require("pg");
 
+const { Pool } = require("pg");
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // MUST for Supabase on Render
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
